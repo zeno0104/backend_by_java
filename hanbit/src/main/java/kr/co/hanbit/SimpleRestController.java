bd2f3e8b9d9edc580d96c15374c81e281d5591be
zenoo97 <jaehun4086@naver.com>
2025-05-21 16:50:24 +0900

    [Update] ajax

---
+++ hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -0,0 +1,18 @@
+package kr.co.hanbit;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title,
+                                @RequestParam("content") String content) {
+        return String.format("title= %s / content= %s", title, content);
+    }
+}
